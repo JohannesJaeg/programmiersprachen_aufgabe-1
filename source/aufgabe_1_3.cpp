dac93f5dd309a80c07cd74e6c7e698a4bb3e7883
JohannesJaeg <jojo.jaeger99@gmail.com>
2019-04-10 18:13:53 +0200

    aufgabe 1.3

--- source/aufgabe_1_3.cpp
+++ source/aufgabe_1_3.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 
+int Division(){
+	int Zahl = 20;
+	bool gefunden = false;
+	while ( gefunden == false) {
+		for (int i = 1; i <= 20; i++) {
+			if (Zahl % i != 0) {
+				break;
+			}
+			if (i == 20) {
+				return Zahl;
+			}
+		}
+		Zahl += 20;
+	}
+};
 int main()
 {
-  std::cout << "Hello, World!\n";
-  return 0;
+	std::cout<<Division();
 }
