18e44747790dbb791b31b6bbbdf7daa432a62015
JohannesJaeg <jojo.jaeger99@gmail.com>
2019-04-12 12:10:34 +0200

    test 6

--- source/tests.cpp
+++ source/tests.cpp
@@ -2,14 +2,9 @@
 #include "catch.hpp"
 #include <cmath>
 
-int gcd(int a, int b) {
-	return 1;
-}
-TEST_CASE("describe_gcd", "[gcd]") {
-	REQUIRE(gcd(2, 4) == 2);
-	REQUIRE(gcd(9, 6) == 3);
-	REQUIRE(gcd(3, 7) == 1);
-}
+//int gcd(int a, int b) {
+//	return 1;
+//}
 
 int is_prime() {
 	int Zahl = 11;
@@ -69,9 +64,9 @@ int fract() {
 	
 }
 
-int zylinder() {
-	float r = 3.231;
-	float h = 5.32;
+int zylinder(float r, float h) {
+	//float r = 3.231;
+	//float h = 5.32;
 	float pi = 3.14159265358979323846;
 	float V, A;
 	V = pi * (2 * r) * h;
@@ -80,8 +75,8 @@ int zylinder() {
 	return 0;
 }
 
-int factorial() {
-	int Zahl = 5;
+int factorial(int Zahl) {
+	//int Zahl = 5;
 	long factor = 1;
 	for (int i = 1; i <= Zahl; i++) {
 		factor *= i;
@@ -89,13 +84,39 @@ int factorial() {
 	return factor;
 }
 
+int gcd(int x, int y) {
+	//int x = 0;
+	//int y = 0;
+
+	assert(x > y || x != 0 && y != 0); // x darf nicht kleiner als y sein
+	//assert(x == 0 && y == 0);
+	if (x == 0) {
+		return y;
+		y %= x;
+		if (y == 0) {
+			return x;
+			x %= y;
+		}
+	}
+}
+
+TEST_CASE("describe_gcd", "[gcd]") {
+	//REQUIRE(gcd(2, 4) == 2);
+	//REQUIRE(gcd(9, 6) == 3);
+	//REQUIRE(gcd(3, 7) == 1);
+	REQUIRE(factorial(5) == 100);
+
+}
+
+
 int main(int argc, char* argv[])
 {
-	//return Catch::Session().run(argc, argv);
+	return Catch::Session().run(argc, argv);
 	  //std::cout << is_prime();
 	  //std::cout << "Quersumme ist " << checksum();
 	//std::cout << "Die Summe der Zahlen von 1 bis 1000, die durch 3 oder durch 5 teilbar sind, betraegt " << sum_multiples();
 	//std::cout << fract();
 	//std::cout << zylinder();
-	std::cout << "Die Fakultaet der Zahl betraegt " << factorial();
+	//std::cout << "Die Fakultaet der Zahl betraegt " << factorial();
+	//std::cout << "Der groeßte gemeinsame Teiler betraegt " << gcd();
 }
